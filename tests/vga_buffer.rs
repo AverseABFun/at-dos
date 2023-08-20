21b731052ef3e59f708c32f1bb955395a06db6a7
Arthur Beck <averse.abfun@gmail.com>
2023-08-20 15:57:00 -0500

    First p0st!! haha

---
+++ tests/vga_buffer.rs
@@ -0,0 +1,31 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(at_dos::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use at_dos::{print, println};
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    at_dos::test_panic_handler(info)
+}
+
+
+#[test_case]
+fn serial_prints() {
+    print!("Print works");
+}
+
+#[test_case]
+fn serial_printlns() {
+    println!("Println works");
+}
